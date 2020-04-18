cf408e21ea7f4f797946567ce054cb514e4b9c5f
mirzayogy <mirza.yogy@gmail.com>
2020-04-18 18:38:05 +0800

    Output dengan sout

--- Modul01/DataTypes.java
+++ Modul01/DataTypes.java
@@ -10,6 +10,14 @@ public class DataTypes{
   char nilaiAbjad = 'A';
   boolean tampan = true;
 
+  System.out.println("Nama depan : " + namaDepan);
+  System.out.println("Nama tengah : " + namaTengah);
+  System.out.println("Nama belakang : " + namaBelakang);
+  System.out.println("Usia : " + usia);
+  System.out.println("Target Kuliah : " + targetTahunKuliah + " tahun");
+  System.out.println("IPK : " + ipk);
+  System.out.println("Nilai PBO : " + nilaiAbjad);
+  System.err.println("Tampan : " + tampan);
  }
 }
 
