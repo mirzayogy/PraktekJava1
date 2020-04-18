dac4ce1964a4fbe290776a03aa37bb5192ca84e0
mirzayogy <mirza.yogy@gmail.com>
2020-04-18 18:03:48 +0800

    deklarasi variabel dan tipe data

---
+++ Modul01/DataTypes.java
@@ -0,0 +1,15 @@
+public class DataTypes{
+ public static void main(String[] args){
+  
+  String namaDepan = "Mirza";
+  String namaTengah = "Yogy";
+  String namaBelakang = "Kurniawan";
+  int usia = 18;
+  int targetTahunKuliah = 4;
+  double ipk = 3.89764512;
+  char nilaiAbjad = 'A';
+  boolean tampan = true;
+
+ }
+}
+
