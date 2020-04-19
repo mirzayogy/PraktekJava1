8214de4b499f032c5ad27687b9e63cb2c335d96d
mirzayogy <mirza.yogy@gmail.com>
2020-04-19 11:57:19 +0800

    Project IDEA

---
+++ Modul01/Modul01_IDEA/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        String namaDepan = "Mirza";
+        String namaTengah = "Yogy";
+        String namaBelakang = "Kurniawan";
+        int usia = 18;
+        int targetTahunKuliah = 4;
+        double ipk = 3.89764512;
+        char nilaiAbjad = 'A';
+        boolean tampan = true;
+
+        String namaLengkap = namaDepan + " " +
+                namaTengah + " " + namaBelakang;
+        System.out.println(namaLengkap);
+                
+    }
+}
+
+
