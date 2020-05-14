9ab10ae2ddd04ede9fe7909510896fa1b101335e
mirzayogy <mirza.yogy@gmail.com>
2020-05-14 11:34:37 +0800

    Menambahkan table dari database

--- Modul04/src/helper/MyConnection.java
+++ Modul04/src/helper/MyConnection.java
@@ -25,44 +25,5 @@ public class MyConnection {
             e.printStackTrace();
             return null;
         }
-
-    }
-
-    public static void main(String[] args) {
-        Matakuliah matakuliah;
-        ArrayList<Matakuliah> matakuliahList = new ArrayList<>();
-        MyConnection myConnection = new MyConnection();
-        Connection con = myConnection.getConnection();
-
-        String selectQuery = "Select * FROM matakuliah ";
-        Statement statement;
-        ResultSet resultSet;
-
-        try{
-            statement = con.createStatement();
-            resultSet =statement.executeQuery(selectQuery);
-            while(resultSet.next()){
-                matakuliah = new Matakuliah(
-                        resultSet.getInt("id_matakuliah"),
-                        resultSet.getString("nama_matakuliah"),
-                        resultSet.getString("singkatan_matakuliah"),
-                        resultSet.getString("nama_dosen"),
-                        resultSet.getString("kontak_dosen"),
-                        resultSet.getBoolean("aktif")
-                );
-                matakuliahList.add(matakuliah);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        for (Matakuliah value : matakuliahList) {
-            System.out.print(value.getIdMatakuliah());
-            System.out.print("\t");
-            System.out.print(value.getSingkatanMatakuliah());
-            System.out.print("\t");
-            System.out.print(value.getNamaDosen());
-            System.out.println();
-        }
     }
 }
