b2385fc4fd3d09f678d4c067a6769490cf316f39
mirzayogy <mirza.yogy@gmail.com>
2020-05-14 09:58:46 +0800

    model Matakuliah + atribut

---
+++ Modul04/src/model/Matakuliah.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Matakuliah {
+
+    int idMatakuliah;
+    String namaMatakuliah;
+    String singkatanMatakuliah;
+    String namaDosen;
+    String kontakDosen;
+    boolean aktif;
+    
+}
