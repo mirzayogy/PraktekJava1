a8c7cec4c5511990b4fc81e39ffdcd4c1821460d
mirzayogy <mirza.yogy@gmail.com>
2020-05-14 12:27:27 +0800

    Mengisi atribut

--- Modul02/src/model/MahasiswaBeraksi.java
+++ Modul02/src/model/MahasiswaBeraksi.java
@@ -1,7 +1,12 @@
 package model;
 
+import java.util.Date;
+
 public class MahasiswaBeraksi {
     public static void main(String[] args) {
-        
+        Mahasiswa mahasiswa = new Mahasiswa();
+        mahasiswa.npm = "19639999";
+        mahasiswa.nama = "Sembilu";
+        mahasiswa.tanggalLahir = new Date();
     }
 }
