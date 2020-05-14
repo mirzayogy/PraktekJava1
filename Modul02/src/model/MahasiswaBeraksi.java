0320423ba2bff0577f23fa452e5c9366918c58a4
mirzayogy <mirza.yogy@gmail.com>
2020-05-14 12:58:09 +0800

    Tanggal Lahir

--- Modul02/src/model/MahasiswaBeraksi.java
+++ Modul02/src/model/MahasiswaBeraksi.java
@@ -1,13 +1,19 @@
 package model;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class MahasiswaBeraksi {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
+
+        String polaTanggal = "dd-MM-yyyy";
+        SimpleDateFormat simpleDateFormat =new SimpleDateFormat(polaTanggal);
+
         Mahasiswa mahasiswa = new Mahasiswa();
         mahasiswa.npm = "19639999";
         mahasiswa.nama = "Sembilu";
-        mahasiswa.tanggalLahir = new Date();
+        mahasiswa.tanggalLahir = simpleDateFormat.parse("09-09-2009");
 
         System.out.println("Nama \t\t\t : " + mahasiswa.nama);
         System.out.println("NPM \t\t\t : " + mahasiswa.npm);
@@ -16,7 +22,7 @@ public class MahasiswaBeraksi {
         Mahasiswa mahasiswa2 = new Mahasiswa();
         mahasiswa2.npm = "19638888";
         mahasiswa2.nama = "Delapu";
-        mahasiswa2.tanggalLahir = new Date();
+        mahasiswa2.tanggalLahir = simpleDateFormat.parse("08-08-2008");
 
         System.out.println("Nama \t\t\t : " + mahasiswa2.nama);
         System.out.println("NPM \t\t\t : " + mahasiswa2.npm);
