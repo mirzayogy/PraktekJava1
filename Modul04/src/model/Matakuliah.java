c889ad2e67e9d3c0c30a43dae00f55f83615067d
mirzayogy <mirza.yogy@gmail.com>
2020-05-14 09:59:43 +0800

    Generate Constructor

--- Modul04/src/model/Matakuliah.java
+++ Modul04/src/model/Matakuliah.java
@@ -8,5 +8,15 @@ public class Matakuliah {
     String namaDosen;
     String kontakDosen;
     boolean aktif;
+
+    public Matakuliah(int idMatakuliah, String namaMatakuliah, String singkatanMatakuliah, String namaDosen, String kontakDosen, boolean aktif) {
+        this.idMatakuliah = idMatakuliah;
+        this.namaMatakuliah = namaMatakuliah;
+        this.singkatanMatakuliah = singkatanMatakuliah;
+        this.namaDosen = namaDosen;
+        this.kontakDosen = kontakDosen;
+        this.aktif = aktif;
+    }
+
     
 }
