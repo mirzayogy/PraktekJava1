629d542126a9a6f3fd8fd51d7dd528b738cb1033
mirzayogy <mirza.yogy@gmail.com>
2020-05-14 13:05:03 +0800

    method tampilkan atribut

--- Modul02/src/model/MahasiswaBeraksi.java
+++ Modul02/src/model/MahasiswaBeraksi.java
@@ -15,17 +15,13 @@ public class MahasiswaBeraksi {
         mahasiswa.nama = "Sembilu";
         mahasiswa.tanggalLahir = simpleDateFormat.parse("09-09-2009");
 
-        System.out.println("Nama \t\t\t : " + mahasiswa.nama);
-        System.out.println("NPM \t\t\t : " + mahasiswa.npm);
-        System.out.println("Tanggal Lahir \t : " + simpleDateFormat.format(mahasiswa.tanggalLahir));
+        mahasiswa.tampilkanAtribut();
 
         Mahasiswa mahasiswa2 = new Mahasiswa();
         mahasiswa2.npm = "19638888";
         mahasiswa2.nama = "Delapu";
         mahasiswa2.tanggalLahir = simpleDateFormat.parse("08-08-2008");
 
-        System.out.println("Nama \t\t\t : " + mahasiswa2.nama);
-        System.out.println("NPM \t\t\t : " + mahasiswa2.npm);
-        System.out.println("Tanggal Lahir \t : " + simpleDateFormat.format(mahasiswa2.tanggalLahir));
+        mahasiswa2.tampilkanAtribut();
     }
 }
